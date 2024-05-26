b5af475f3c55c48910ffb5af10a736eb64d71a11
frenziedcode <contact.viniciuslopes@gmail.com>
2024-05-26 18:27:43 +0100

    update

--- javascript/modal/no_outro_dia_modal.js
+++ javascript/modal/no_outro_dia_modal.js
@@ -1,57 +1,73 @@
-const images = [
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_1.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_2.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_3.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_4.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_4a.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_5.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_6.jpg",
-    "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_7.jpg"
-    
-    // Adicione os caminhos das suas imagens aqui
-];
+document.addEventListener("DOMContentLoaded", function() {
+    // Desativar a seleção de texto na página
+    document.body.style.userSelect = "none";
+    document.body.style.webkitUserSelect = "none"; // Para navegadores baseados no WebKit
+    document.body.style.msUserSelect = "none"; // Para navegadores baseados no Trident/Edge
 
-let currentImageIndex = 0;
-const currentImgElement = document.getElementById("current-img");
+    // Desativar o clique com o botão direito na página
+    document.addEventListener("contextmenu", function(event) {
+        event.preventDefault();
+    });
 
-// Função para exibir a imagem atual
-function showCurrentImage() {
-    currentImgElement.src = images[currentImageIndex];
-}
+    const images = [
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_1.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_2.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_3.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_4.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_4a.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_5.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_6.jpg",
+        "../imgs/installation_views/no_outro_dia/viniciuslopes_no_outro_dia_installationview_7.jpg"
+    ];
 
-// Event listener para avançar para a próxima imagem
-currentImgElement.addEventListener("click", function(event) {
-    const x = event.clientX;
-    const imgWidth = currentImgElement.offsetWidth;
-    if (x > imgWidth / 2) {
-        nextImage();
-    } else {
-        prevImage();
+    let currentImageIndex = 0;
+    const currentImgElement = document.getElementById("current-img");
+
+    // Função para exibir a imagem atual
+    function showCurrentImage() {
+        currentImgElement.src = images[currentImageIndex];
     }
+
+    // Event listener para avançar para a próxima imagem
+    currentImgElement.addEventListener("click", function(event) {
+        const x = event.clientX;
+        const imgWidth = currentImgElement.offsetWidth;
+        if (x > imgWidth / 2) {
+            nextImage();
+        } else {
+            prevImage();
+        }
+    });
+
+    // Função para avançar para a próxima imagem
+    function nextImage() {
+        currentImageIndex++;
+        if (currentImageIndex >= images.length) {
+            currentImageIndex = 0;
+        }
+        showCurrentImage();
+    }
+
+    // Função para voltar para a imagem anterior
+    function prevImage() {
+        currentImageIndex--;
+        if (currentImageIndex < 0) {
+            currentImageIndex = images.length - 1;
+        }
+        showCurrentImage();
+    }
+
+    // Event listeners para os botões "prev" e "next"
+    document.getElementById("next-btn").addEventListener("click", nextImage);
+    document.getElementById("prev-btn").addEventListener("click", prevImage);
+    document.getElementById("next-btn-mobile").addEventListener("click", nextImage);
+    document.getElementById("prev-btn-mobile").addEventListener("click", prevImage);
+
+    // Função para lidar com erros de carregamento da imagem
+    function handleImageError() {
+        console.error("Erro ao carregar a imagem:", images[currentImageIndex]);
+    }
+
+    // Exibir a primeira imagem ao carregar a página
+    showCurrentImage();
 });
-
-// Função para avançar para a próxima imagem
-function nextImage() {
-    currentImageIndex++;
-    if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
-    }
-    showCurrentImage();
-}
-
-// Função para voltar para a imagem anterior
-function prevImage() {
-    currentImageIndex--;
-    if (currentImageIndex < 0) {
-        currentImageIndex = images.length - 1;
-    }
-    showCurrentImage();
-}
-
-// Função para lidar com erros de carregamento da imagem
-function handleImageError() {
-    console.error("Erro ao carregar a imagem:", images[currentImageIndex]);
-}
-
-// Exibir a primeira imagem ao carregar a página
-showCurrentImage();
